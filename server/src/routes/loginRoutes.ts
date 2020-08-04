8525492e584ccb5c1880e37176fdc59020efbf18
rakesh.cheekatimala <rakeshcheekatimala@gmail.com>
2020-08-04 08:55:06 +0800

    adding requireAuth for protected routes

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,9 +1,19 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 const router = Router();
 
+const requireAuth = (req:Request,res:Response,next:NextFunction) =>{
+  if(req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+  else {
+    res.status(403);
+    res.send('You are not allowed perform this operation');
+  }
+}
 router.get('/login', (req: Request, res: Response) => {
   return res.send(`
     <form method="post">
@@ -57,4 +67,7 @@ router.get('/logout',(req:Request,res:Response)=>{
   res.redirect('/');
 });
 
+router.get('/protected',requireAuth,(req:Request,res:Response)=>{
+  res.send('You can access the protected route since you are logged in successfully')
+})
 export default router;
